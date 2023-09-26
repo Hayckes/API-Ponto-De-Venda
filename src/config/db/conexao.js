00c47df0f2c41ecc8d30478e9fbea03e70a520e0
Ygor Guilherme <ygorfsguilherme@gmail.com>
2023-09-26 20:21:43 -0300

    estrutura do projeto

---
+++ src/config/db/conexao.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_PORT
+} = process.env
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : DB_HOST,
+      port : DB_PORT,
+      user : DB_USER,
+      password : DB_PASSWORD,
+      database : DB_DATABASE
+    }
+  });
+
+module.exports = knex
