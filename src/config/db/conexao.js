cbf66d1009f2d1cffd411329735fc70d233dba34
Ygor Guilherme <ygorfsguilherme@gmail.com>
2023-09-27 06:28:56 -0300

    padrão de projeto

--- src/config/db/conexao.js
+++ src/config/db/conexao.js
@@ -1,22 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_PORT
-} = process.env
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host : DB_HOST,
-      port : DB_PORT,
-      user : DB_USER,
-      password : DB_PASSWORD,
-      database : DB_DATABASE
-    }
-  });
+  client: 'pg',
+  connection: {
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+  },
+});
 
-module.exports = knex
+module.exports = knex;
