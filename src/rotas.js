e16bbf0be641a5b9282d14efca50e47cdb67c6e5
Denis Reis <denispereirareis@gmail.com>
2023-09-28 14:20:45 -0300

    Cadastro ok

--- src/rotas.js
+++ src/rotas.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const perfilDetalhar = require("./controladores/perfilDetalhar");
+const intermediarioEmail = require("./intermediarios/usuario");
+const cadastroUsuarios = require("./controladores/cadastroUsuario");
 
 const rotas = express();
 
+rotas.post("/usuario", intermediarioEmail, cadastroUsuarios)
 //Detalhar Perfil Usuario
 rotas.get("/usuario/:id", perfilDetalhar);
 
