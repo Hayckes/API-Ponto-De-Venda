e8b47cdfba5136ec0397777ba42cacfde02cadc0
Tiago Lenovo <tiagonevestec@gmail.com>
2023-09-29 19:13:50 -0300

    Update Features 'detalharPerfil'[200~app.get('/', (req, res) => { Create Intermediario 'autenticarUsuario'

--- src/controladores/perfilDetalhar.js
+++ src/controladores/perfilDetalhar.js
@@ -1,5 +1,17 @@
+const detalharPerfil = require("../repositorios/detalharPerfil");
+
 const perfilDetalhar = async (req, res) => {
-  return res.json(req.usuario);
+  const usuario_id = req.usuario_id;
+
+  try {
+    const detalhesUsuario = await detalharPerfil(usuario_id);
+    delete detalhesUsuario[0].senha
+
+    return res.status(200).json(detalhesUsuario[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
+
 
 module.exports = perfilDetalhar;
--- src/rotas.js
+++ src/rotas.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
 const rotas = express.Router();
 
-const usuarioLogin = require('./controladores/usuarioLogin');
-const perfilDetalhar = require("./controladores/perfilDetalhar");
-const intermediarioEmail = require("./intermediarios/usuario");
 const cadastroUsuarios = require("./controladores/cadastroUsuario");
+const usuarioLogin = require("./controladores/usuarioLogin");
+const perfilDetalhar = require("./controladores/perfilDetalhar");
 
-rotas.post('/login', usuarioLogin);
+const intermediarioEmail = require("./intermediarios/usuario");
+const autenticarUsuario = require("./intermediarios/autenticarUsuario");
 
-rotas.post("/usuario", intermediarioEmail, cadastroUsuarios)
-//Detalhar Perfil Usuario
-rotas.get("/usuario/:id", perfilDetalhar);
+rotas.get('/', (req, res) => { res.send('Desafio M05') });
+rotas.post("/usuario", intermediarioEmail, cadastroUsuarios);
+rotas.post("/login", usuarioLogin);
+
+//Intermediario ( Autenticador )
+rotas.use(autenticarUsuario);
+
+rotas.get("/usuario", perfilDetalhar);
 
 module.exports = rotas;
