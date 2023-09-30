9f87b9d5bd1848b9ad6a683388f5a777d6f07438
Ravel Levi <116118813+Hayckes@users.noreply.github.com>
2023-09-30 20:36:07 -0300

    criando rota de atualizar dados do usuario

---
+++ src/config/db/jwt.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+function verificarJwt(token) {
+  try {
+    const secret_key = process.env.JWT_HASH;
+    const decoded = jwt.verify(token, secret_key);
+    return decoded;
+  } catch (error) {
+    console.log(JSON.stringify(error));
+    return null;
+  }
+}
+
+module.exports = {
+
+  verificarJwt,
+};
