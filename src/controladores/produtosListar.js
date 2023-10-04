f50e92091b448949f1aec7fafc4771e55c4ecb19
Tiago Lenovo <tiagonevestec@gmail.com>
2023-10-04 20:35:30 -0300

    Atualização da Rota Listar Produtos

--- src/controladores/produtosListar.js
+++ src/controladores/produtosListar.js
@@ -1,11 +1,9 @@
 const listarProdutos = require('../repositorios/listarProdutos');
 
-//listar produtos por categoria_id e/ou todos
 const produtosListar = async (req, res) => {
   const categoria_id = req.query;
 
   try {
-    // Verifique se o parâmetro categoria_id foi fornecido
     if (categoria_id) {
       const produtosFiltrados = await listarProdutos(categoria_id);
       return res.status(200).json(produtosFiltrados);
@@ -14,7 +12,8 @@ const produtosListar = async (req, res) => {
 
     return res.status(200).json(listaDeCategorias);
   } catch (error) {
-    return res.status(500).json({ mensagem: 'TEST Erro interno no servidor' });
+    console.log(error);
+    return res.status(500).json({ mensagem: 'Erro interno no servidor' });
   }
 };
 
