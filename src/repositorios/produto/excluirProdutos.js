705d7d82941c59f2f1e3c7fadfd913be010deb63
Ravel Levi <116118813+Hayckes@users.noreply.github.com>
2023-10-11 15:07:33 -0300

    escluindo imagme do produto

--- src/repositorios/produto/excluirProdutos.js
+++ src/repositorios/produto/excluirProdutos.js
@@ -1,7 +1,7 @@
 const knex = require('../../config/db/conexao');
 
-const excluirProdutos = ({ id }) => {
-  const produtoExcluido = knex('produtos').where('id', id).del();
+const excluirProdutos = (id) => {
+  const produtoExcluido = knex('produtos').where(id).del();
 
   return produtoExcluido;
 };
